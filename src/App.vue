8381effe518d859a91c7a7d3f16f275af0b8002a
xin1126 <z26319491z@gmail.com>
2022-10-25 13:05:57 +0800

    feat: google font文字

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,6 @@
 <style>
 #app {
   @apply bg-backound min-h-screen;
+  font-family: 'Squada One', 'Noto Sans TC', -apple-system, BlinkMacSystemFont, "Segoe UI", "Microsoft JhengHei", "Helvetica Neue", Arial, system-ui, cursive, sans-serif;
 }
 </style>
