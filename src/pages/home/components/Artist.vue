03d7d0e04813b50eb2362fa54414a54f9cbb09a5
xin1126 <z26319491z@gmail.com>
2022-10-28 14:07:52 +0800

    feat: Swiper page

--- src/pages/home/components/Artist.vue
+++ src/pages/home/components/Artist.vue
@@ -8,8 +8,34 @@ const onSlideChange = ({ activeIndex }: { activeIndex: number }) => {
   swiperSlideTarget.value = activeIndex
 }
 
-const onSwiper = (swiper: SwiperCore) => {
-  swiper.activeIndex = 1
+const innerWidth = ref(0)
+const resetSwiperMobile = ref(0)
+const resetSwiperPC = ref(0)
+innerWidth.value = window.innerWidth
+window.onresize = () => {
+  innerWidth.value = window.innerWidth
+  if (innerWidth.value < 1280) {
+    swiperSlideTarget.value = 0
+    resetSwiperPC.value++
+  }
+  else {
+    swiperSlideTarget.value = 1
+    resetSwiperMobile.value++
+  }
+}
+
+const onSwiperMobile = (swiper: SwiperCore) => {
+  if (innerWidth.value < 1280) {
+    swiperSlideTarget.value = 0
+    swiper.activeIndex = 0
+  }
+}
+
+const onSwiperPC = (swiper: SwiperCore) => {
+  if (innerWidth.value > 1280) {
+    swiperSlideTarget.value = 1
+    swiper.activeIndex = 1
+  }
 }
 
 const swiperRight = computed(() => swiperSlideTarget.value === 0 ? '100px' : swiperSlideTarget.value === 1 ? '0' : `${-80 * swiperSlideTarget.value}px`)
@@ -23,10 +49,11 @@ const swiperSlideStyle = (item: number) => item - 1 === swiperSlideTarget.value 
       <Button class="relative bottom-[-13px]" :border="false" />
     </div>
     <Swiper
+      :key="resetSwiperPC"
       :slides-per-view="3"
       :space-between="10"
-      class="hidden xl:block"
-      @swiper="onSwiper"
+      class="onSwiperPC hidden xl:block"
+      @swiper="onSwiperPC"
       @slide-change="onSlideChange"
     >
       <SwiperSlide class="opacity-0" />
@@ -36,17 +63,24 @@ const swiperSlideStyle = (item: number) => item - 1 === swiperSlideTarget.value 
       <SwiperSlide class="opacity-0" />
     </Swiper>
     <Swiper
+      :key="resetSwiperMobile"
       :space-between="10"
+      class="xl:hidden"
+      @swiper="onSwiperMobile"
+      @slide-change="onSlideChange"
     >
-      <SwiperSlide v-for="item in 5" :key="item" class="xl:hidden">
+      <SwiperSlide v-for="item in 5" :key="item">
         <img :src="`/images/artist0${item}.jpg`" alt="art" class="h-[300px] w-full">
       </SwiperSlide>
     </Swiper>
+    <div class="mt-6 flex justify-center xl:mt-12">
+      <div v-for="item in 5" :key="item" class="mr-2 h-[8px] w-[8px] bg-black" :class="[swiperSlideTarget === item - 1 ? 'w-[16px] !bg-primary' : '']" />
+    </div>
   </div>
 </template>
 
 <style>
-.swiper-wrapper {
+.onSwiperPC .swiper-wrapper {
   align-items: center;
   right: v-bind(swiperRight);
 }
