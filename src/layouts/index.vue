2a9623c99a31f7628c916dd0284f83626f6b03e3
xin1126 <z26319491z@gmail.com>
2022-12-06 14:08:23 +0800

    fix: layout margin

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -5,7 +5,7 @@ watch(() => route.path, () => document.documentElement.scrollTop = 0)
 
 <template>
   <Header />
-  <main class="pt-20 md:mb-20 lg:pt-24 xl:pt-36">
+  <main class="pt-20 lg:pt-24 xl:pt-36" :class="{ 'md:mb-20': route.path !== '/home' }">
     <router-view />
   </main>
   <Footer />
