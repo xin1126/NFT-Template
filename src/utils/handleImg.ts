5977e4c8588942bf37e22322cbbf941d8217e3c7
xin1126 <z26319491z@gmail.com>
2022-12-07 18:50:24 +0800

    fix: handleImg 線上環境路徑問題

--- src/utils/handleImg.ts
+++ src/utils/handleImg.ts
@@ -1,1 +1,8 @@
-export default (img: string) => new URL(`../assets/images/${img}`, import.meta.url).href
+export default (img: string) => {
+  if (!import.meta.env.DEV && img.includes('/')) { // 處理非開發環境路徑
+    const tempImg = img.split('/')
+    img = tempImg[tempImg.length - 1]
+  }
+
+  return new URL(`../assets/images/${img}`, import.meta.url).href
+}
